8aeb2a534e9432c80cc131f68bfed0f9792dc005
YwT <1181339151@qq.com>
2018-11-24 20:22:50 +0800

    [add] 搭建WebSocket

---
+++ src/main/java/com/ywt/os/SpringWebSocketConfiguration.java
@@ -0,0 +1,29 @@
+package com.ywt.os;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author: YwT
+ * @description: Web Socket 配置
+ * @create: 2018-11-24 19:58
+ **/
+@Configuration
+@EnableWebSocketMessageBroker
+public class SpringWebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+
+}
